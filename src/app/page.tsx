008210d693f0fccecb2ba6a0beb0d182da4cc99f
Dan Valentin Nichita <161126392+danvalentin-nichita@users.noreply.github.com>
2025-08-24 21:22:59 +0200

    refactor image handling in HomePage and update database schema for images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,18 @@
-import Link from "next/link";
+import { db } from '~/server/db';
 
+export const dynamic = 'force-dynamic';
 
-const mockUrls = [
-  "https://xvwfyga4x0.ufs.sh/f/fSXVckr1wnt47dtZ7vV2FlBOt8QVqI4iP9gRoDJsW1ypGnfm",
-  "https://xvwfyga4x0.ufs.sh/f/fSXVckr1wnt4nvd2KSwithZGay5ew9DqU7Xk4lfbBOJFPTzd",
-  "https://xvwfyga4x0.ufs.sh/f/fSXVckr1wnt4LVaa6l4cHywVUMd4bD6vkuRZgKmelTzs1Xp3",
-  "https://xvwfyga4x0.ufs.sh/f/fSXVckr1wnt4YF9JzvQBkpNDvuLyhO95WEPXgGro4JisCnw8",
-]
+export default async function HomePage() {
+  const images = await db.query.images.findMany();
+  console.log(images);
 
-const mockImages = mockUrls.map((url, index) => ({
-  id: index+1,
-  url,
-}));
-
-export default function HomePage() {
   return (
     <main className="">
       <div className="flex flex-wrap">
-        {mockImages.map((image) => (
+        {[...images, ...images, ...images].map((image) => (
           <div key={image.id} className="w-48">
               <img src={image.url} alt={`Image ${image.id}`} className="w-full h-auto" />
+              <div>{image.name}</div>
           </div>))}
           </div>
       Hello (gallery in progress)
